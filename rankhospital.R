781f396a696225abe73a7c28d045d709e67adab2
igorekymailcom <igorek@ymail.com>
2014-07-01 00:19:23 +0400

    v 0.2  01072014

---
+++ rankhospital.R
@@ -0,0 +1,31 @@
+rankhospital <- function(state, outcome, num = "best"){
+  out <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ##Rate <- as.numeric(out[out[,11] != "Not Available", 11])
+  
+  out[, 11] <- as.numeric(out[, 11])
+  out[, 17] <- as.numeric(out[, 17])
+  out[, 23] <- as.numeric(out[, 23])
+  
+  if((state %in% names(table(out$State))) == FALSE)
+    stop("invalid state")
+  
+  if((outcome %in% c("heart attack", "heart failure", "pneumonia")) == FALSE)
+    stop("invalid outcome")
+  
+  st<-out[out[,7]==state,]
+  
+  if (outcome == "heart attack"){st<-st[order(st$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack),] 
+                                 ;data2 <-st[,2];}
+  else if (outcome == "heart failure") {st<-st[order(st$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure),] ;data2 <-st[,2];}
+  else {st<-st[order(st$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),];data2 <-st[,2];}
+  
+  l<-length(data2) ;
+  if(num =="best") {ho<-sort(data2)[1];return(as.character(ho));}
+  if(num =="worst") {ho<-sort(data2)[l];return(as.character(ho));}
+  if(num>l) {ho<-NA; return (ho);}
+   ho<-sort(data2)[num];
+  ##ho<-sort(as.numeric(as.character(data2)))[1];
+  return(as.character(ho))
+  
+  ##return(data2)
+}
